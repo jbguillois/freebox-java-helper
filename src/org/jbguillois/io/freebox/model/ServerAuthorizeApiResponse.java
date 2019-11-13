10a8cc4875899cbea60a888d781560f84ab06b33
Jean-Baptiste Guillois <jeanbaptiste@synaptyc.io>
2019-11-13 12:00:16 +0100

    Source hierarchy cleanup, forced java 1.8 compilation

--- src/org/jbguillois/io/freebox/model/ServerAuthorizeApiResponse.java
+++
@@ -1,9 +0,0 @@
-package org.jbguillois.io.freebox.model;
-
-public class ServerAuthorizeApiResponse extends ServerApiResponse {
-	private AuthorizeResponse result;
-	
-	public AuthorizeResponse getResult() {
-		return result;
-	}
-}
--- src/org/jbguillois/io/freebox/model/ServerCreateSessionApiResponse.java
+++
@@ -1,9 +0,0 @@
-package org.jbguillois.io.freebox.model;
-
-public class ServerCreateSessionApiResponse extends ServerApiResponse {
-	private CreateSessionResponse result;
-	
-	public CreateSessionResponse getResult() {
-		return result;
-	}
-}
